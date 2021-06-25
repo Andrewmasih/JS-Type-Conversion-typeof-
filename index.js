003349f9c886f8ecc9d0bf279e433d55ffc2dcae
Andrew Masih <armasih@outlook.com>
2021-06-25 22:39:37 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,31 @@
+/* this is useful for Debugging whne you need to know if something is actually the type you expected it to be. 
+*/
+
+let digit = 123;
+
+let firstName = "Peter";
+
+let educated = true;
+
+let undefinedvarible;
+
+let placeholder = null;
+
+let daysOfTheWeek = ["sunday", "monday", "tuesday", "wednesday", "thursday", "friday", "saturday"];
+
+let person = {
+  name: "peter",
+  age: "21"
+};
+function sayHello() {
+  console.log("hello everyone");
+}
+
+console.log(typeof (digit));
+console.log(typeof (firstName));
+console.log(typeof (educated));
+console.log(typeof (undefinedvarible));
+console.log(typeof (placeholder));
+console.log(typeof (daysOfTheWeek));
+console.log(typeof (person));
+console.log(typeof (sayHello));
